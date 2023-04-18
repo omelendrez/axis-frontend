5a0596017a15c69707f0493d32ca68729574a1b5
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-18 14:11:46 -0300

    Add eslint

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa"
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA(), eslint()],
   server: {
     host: true
   }
