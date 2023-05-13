55da3766c47feab793ebc68e77b1859a7e4c0bbc
Omar Melendrez <omar.melendrez@gmail.com>
2023-05-13 14:34:05 -0300

    Implement schema for role form

--- src/components/role/Role.jsx
+++ src/components/role/Role.jsx
@@ -1,23 +1,21 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { InputField, FormButtonRow, SaveButton, CancelButton } from '../shared'
-import { createRole, updateRole } from '../../services'
-import useNotification from '../../hooks/useNotification'
+import { Form } from '../shared'
 
-const initialValues = {
-  name: {
-    value: '',
-    error: ''
-  }
-}
+import useRoles from '../../hooks/useRoles'
+
+import schema from './schema.json'
+import { loadSchema } from '../../helpers'
 
 export const Role = ({ role }) => {
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const { roles, add, modify } = useRoles()
+  const { isLoading, isSuccess } = roles
+
+  const initialValues = loadSchema(schema)
+
   const [values, setValues] = useState(initialValues)
-  const { set } = useNotification()
-  const isMounted = useRef(true)
+
   const navigate = useNavigate()
-  const formRef = useRef()
 
   useEffect(() => {
     if (role) {
@@ -34,74 +32,25 @@ export const Role = ({ role }) => {
     setValues((values) => ({ ...values, [id]: data }))
   }
 
-  const handleApiSuccess = (message) => {
-    isMounted.current = false
-    const notification = { type: 'success', message }
-    set(notification)
-    navigate(-1)
-  }
-
-  const handleApiError = (e) => {
-    const message =
-      e.code === 'ERR_BAD_REQUEST'
-        ? 'Some fields have wrong information. Please double-check and try again.'
-        : e.message
-    const notification = {
-      type: 'error',
-      message
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(-1)
     }
-    set(notification)
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSuccess])
 
-  const handleFinally = () => {
-    if (isMounted.current) {
-      setIsSubmitting(false)
-    }
-  }
-
-  const create = (payload) => {
-    createRole(payload)
-      .then(() => {
-        handleApiSuccess('Role added successfully!')
-      })
-      .catch((e) => {
-        handleApiError(e)
-      })
-      .finally(() => {
-        handleFinally()
-      })
-  }
-
-  const update = (payload) => {
-    updateRole(role.id, payload)
-      .then((res) => {
-        handleApiSuccess('Role saved successfully')
-      })
-      .catch((e) => {
-        handleApiError(e)
-      })
-      .finally(() => {
-        handleFinally()
-      })
-  }
-
-  const handleFormSubmit = (e) => {
+  const handleSave = (e) => {
     e.preventDefault()
-    setIsSubmitting(true)
+
     const payload = Object.entries(values)
       .filter((id) => id !== 'id')
       .reduce((acc, [id, value]) => ({ ...acc, [id]: value.value }), {})
 
     if (role?.id) {
-      update(payload)
+      modify(role.id, payload)
     } else {
-      create(payload)
+      add(payload)
     }
-  }
-
-  const handleSave = (e) => {
-    e.preventDefault()
-    formRef.current.submit()
   }
 
   const handleFormCancel = (e) => {
@@ -110,22 +59,14 @@ export const Role = ({ role }) => {
   }
 
   return (
-    <form onSubmit={handleFormSubmit} ref={formRef}>
-      <InputField
-        type="text"
-        id="name"
-        label="Role"
-        placeholder="Enter name"
-        value={values.name.value}
-        onChange={handleChange}
-        required
-      />
-
-      <FormButtonRow>
-        <SaveButton isSubmitting={isSubmitting} onSave={handleSave} />
-
-        <CancelButton isSubmitting={isSubmitting} onCancel={handleFormCancel} />
-      </FormButtonRow>
-    </form>
+    <Form
+      schema={schema}
+      object={role}
+      isLoading={isLoading}
+      onChange={handleChange}
+      values={values}
+      onSave={handleSave}
+      onClose={handleFormCancel}
+    />
   )
 }
