34addfce677259c1da40d7cffa6c9bf02618873a
Omar Melendrez <omar.melendrez@gmail.com>
2023-05-30 06:19:31 -0300

    Re-arrange menu options distribution

--- src/components/home/MenuOption.jsx
+++ src/components/home/MenuOption.jsx
@@ -1,9 +1,13 @@
 import { Divider } from '../shared/divider/Divider'
 
-export const MenuOption = ({ title, description, path, onNavigate, divider }) =>
-  divider ? (
-    <Divider />
-  ) : (
+export const MenuOption = ({
+  title,
+  description,
+  path,
+  onNavigate,
+  divider
+}) => (
+  <>
     <article
       className="home-item"
       onClick={(e) => {
@@ -16,4 +20,6 @@ export const MenuOption = ({ title, description, path, onNavigate, divider }) =>
         <h4>{description}</h4>
       </hgroup>
     </article>
-  )
+    {divider && <Divider />}
+  </>
+)
