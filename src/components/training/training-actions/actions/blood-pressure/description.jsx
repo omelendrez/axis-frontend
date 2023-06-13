302cd61027d64c1e38c297c72ec902fa3a010d09
Omar Melendrez <omar.melendrez@gmail.com>
2023-06-13 18:04:45 -0300

    Improve blood pressure description component

--- src/components/training/training-actions/actions/blood-pressure/description.jsx
+++ src/components/training/training-actions/actions/blood-pressure/description.jsx
@@ -8,16 +8,18 @@ const text = (
   <>
     <ol>
       <li>
-        Enter the systolic and diastolic blood pressure measured in the
+        Enter the systolic and diastolic blood pressure measures in their
         respective fields.
       </li>
       <li>
-        Click on <Ok /> to confirm that the learner is apt to performer the
+        Click on <Ok /> to confirm that the learner is fit to perform the
         training course.
       </li>
       <li>
         Click on <NotOk /> to confirm the learner
         <strong> is NOT FIT to perform the training course </strong>.
+        <br />
+        In this case the learner cannot take the course.
       </li>
       <li>
         <CommonMessage />
