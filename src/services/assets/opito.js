82b87aef0ffbb9c9931032fbf6b9bbd2713d9303
Omar Melendrez <omar.melendrez@gmail.com>
2023-06-25 16:53:31 -0300

    Post opito payload only

--- src/services/assets/opito.js
+++ src/services/assets/opito.js
@@ -2,5 +2,4 @@ import { api } from './assetsClient'
 
 const endpoint = 'opito'
 
-export const generateOpitoCertificate = (id, payload) =>
-  api.post(`${endpoint}/${id}`, payload)
+export const generateOpitoCertificate = (payload) => api.post(endpoint, payload)
