149a01988fc6ada197c5be83205a3e908717e407
Omar Melendrez <omar.melendrez@gmail.com>
2023-07-22 18:28:51 -0300

    Inactivate dates after today for pending tasks calendar

--- src/pages/PendingTasks.jsx
+++ src/pages/PendingTasks.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react'
-import { Loading, Calendar } from '@/components'
-import usePage from '@/hooks/usePage'
-import { formatYMD } from '@/helpers'
 import compareDesc from 'date-fns/compareDesc'
+
+import { Loading, Calendar } from '@/components'
+
+import usePage from '@/hooks/usePage'
+
+import { formatYMD } from '@/helpers'
+
 import './pendingTasks.css'
 
 const PendingTasks = () => {
@@ -22,31 +26,21 @@ const PendingTasks = () => {
     setIsLoading(false)
   }, [date])
 
-  const tileDisabled = ({ date, view }) => {
-    // Disable tiles in month view only
-    if (view === 'month') {
-      // Check if a date React-Calendar wants to check is on the list of disabled dates
-      return compareDesc(new Date(), date) > 0
-    }
-  }
-
   return (
     <main className="container-fluid">
       {isLoading && <Loading />}
       <article className="pending-tasks">
-        <div>
-          <Calendar
-            onChange={setDate}
-            value={date}
-            tileDisabled={tileDisabled}
-          />
-          <button
-            className="calendar-today-button"
-            onClick={() => setDate(new Date())}
-          >
-            Today
-          </button>
-        </div>
+        <Calendar
+          onChange={setDate}
+          value={date}
+          tileDisabled={({ date }) => compareDesc(new Date(), date) > 0}
+        />
+        <button
+          className="calendar-today-button"
+          onClick={() => setDate(new Date())}
+        >
+          Today
+        </button>
       </article>
     </main>
   )
