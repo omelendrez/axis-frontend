82afa7f1fd4ba0c4b343623d1a2cd8af52a7f58b
Omar Melendrez <omar.melendrez@gmail.com>
2023-07-26 19:25:11 -0300

    Add date format methods

--- src/helpers/format.js
+++ src/helpers/format.js
@@ -1,5 +1,14 @@
+import compareDesc from 'date-fns/compareDesc'
+import formatDistance from 'date-fns/formatDistance'
+import format from 'date-fns/format'
+
 export const formatShortDate = (date) =>
   new Intl.DateTimeFormat('en-GB').format(new Date(date))
+
+export const isNewer = (date) => compareDesc(new Date(), date) > 0
+
+export const isEqual = (date) =>
+  formatShortDate(date) === formatShortDate(new Date())
 
 export const formatYMD = (date) =>
   new Intl.DateTimeFormat('en-GB')
@@ -8,8 +17,20 @@ export const formatYMD = (date) =>
     .reverse()
     .join('-')
 
+export const formatFullDate = (date) => {
+  return formatYMD(date) === formatYMD(new Date())
+    ? format(date, 'PPPP')
+    : `${format(date, 'PPPP')} (${formatDistance(
+        new Date(formatYMD(date)),
+        new Date(formatYMD(new Date())),
+        {
+          addSuffix: true
+        }
+      )})`
+}
+
 export const documentNumber = (num) =>
-  (parseInt(num, 10) + 1000000000000).toString().substring(1)
+  (parseInt(num, 10) + (10 ^ 12)).toString().substring(1)
 
 export const signatureNumber = (num) => parseInt(num, 10).toString(16)
 
