195394438021e8460071e76c04efb08dfeeb5bd3
Omar Melendrez <omar.melendrez@gmail.com>
2023-07-26 19:24:41 -0300

    Rename Calendar component prop

--- src/components/shared/calendar/Calendar.jsx
+++ src/components/shared/calendar/Calendar.jsx
@@ -2,11 +2,11 @@ import CalendarComponent from 'react-calendar'
 // import 'react-calendar/dist/Calendar.css'
 import './calendar.css'
 
-export const Calendar = ({ onChange, value, tileDisabled }) => {
+export const Calendar = ({ onChange, date, tileDisabled }) => {
   return (
     <CalendarComponent
       onChange={onChange}
-      value={value}
+      value={date}
       tileDisabled={tileDisabled}
     />
   )
