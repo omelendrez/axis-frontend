cb8ae659694a0738187abcc5dba6f2169a8c09f3
Omar Melendrez <omar.melendrez@gmail.com>
2023-08-30 14:03:33 -0300

    Remove signature from application

--- src/helpers/format.js
+++ src/helpers/format.js
@@ -34,13 +34,8 @@ export const formatFullDate = (date) => {
 export const documentNumber = (num) =>
   (parseInt(num, 10) + 10 ** 12).toString().substring(1)
 
-export const signatureNumber = (num) => parseInt(num, 10).toString(16)
-
 export const getFilename = (id) => `${documentNumber(id)}.pdf`
 
 export const getFOETFilename = (id) => `${documentNumber(id)}.jpg`
-
-export const getSignatureFilename = ({ id, date }) =>
-  `${signatureNumber(`${id}${date}`)}.png`
 
 export const getImageFilename = (badge) => `${badge}.jpg`
