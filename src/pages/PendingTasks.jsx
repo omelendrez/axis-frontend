22e1380c03a6556b4354743877d06a07b1d5bbfc
Omar Melendrez <omar.melendrez@gmail.com>
2023-09-12 12:54:09 -0300

    Refacter multiple useState to make use of useReducer

--- src/pages/PendingTasks.jsx
+++ src/pages/PendingTasks.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from 'react'
+import { useEffect, useState, useContext, useReducer } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import {
@@ -31,37 +31,85 @@ import { approveMultiple, rejectMultiple } from '@/services'
 
 import '../components/features/pending-tasks/pendingTasks.css'
 
+const REDUCER_TYPE = {
+  PAGINATION: 'PAGINATION',
+  SELECTED_ROWS: 'SELECTED_ROWS',
+  SHOW_INPUT_PARAMS: 'SHOW_INPUT_PARAMS',
+  REFRESH: 'REFRESH',
+  SELECTED_RADIO: 'SELECTED_RADIO',
+  AUTHORIZED_STATUSES: 'AUTHORIZED_STATUSES'
+}
+
+const initialState = {
+  pagination: initialValues,
+  selectedRows: [],
+  showInputParameters: false,
+  refresh: false,
+  selectedRadioOption: RADIO.NONE,
+  authorizedStatuses: []
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case REDUCER_TYPE.PAGINATION:
+      return {
+        ...state,
+        pagination: action.payload
+      }
+
+    case REDUCER_TYPE.SELECTED_ROWS:
+      return {
+        ...state,
+        selectedRows: action.payload
+      }
+
+    case REDUCER_TYPE.SHOW_INPUT_PARAMS:
+      return {
+        ...state,
+        showInputParameters: action.payload
+      }
+
+    case REDUCER_TYPE.REFRESH:
+      return {
+        ...state,
+        refresh: action.payload
+      }
+
+    case REDUCER_TYPE.SELECTED_RADIO:
+      return {
+        ...state,
+        selectedRadioOption: action.payload
+      }
+
+    case REDUCER_TYPE.AUTHORIZED_STATUSES:
+      return {
+        ...state,
+        authorizedStatuses: action.payload
+      }
+
+    default:
+      return state
+  }
+}
+
 const PendingTasks = () => {
-  const { pendingTasksParams, setPendingTaksParams } =
-    useContext(PendingTasksContext)
-
-  const { user } = useUser()
-
-  const { date, selectedStatuses } = pendingTasksParams
-
-  const [pagination, setPagination] = useState(initialValues)
-
-  const [selectedRows, setSelectedRows] = useState([])
-
-  const [showInputParameters, setShowInputParameters] = useState(false)
-
-  const [refresh, setRefresh] = useState(false)
-
-  const [radioSelected, setRadioSelected] = useState(RADIO.NONE)
-
   const navigate = useNavigate()
 
+  const { user } = useUser()
+  const { apiMessage } = useApiMessages()
   const { set: setPage } = usePage()
+
+  const { pendingTasksParams, setPendingTaksParams } =
+    useContext(PendingTasksContext)
+  const { date, selectedStatuses } = pendingTasksParams
 
   const { statuses, load: loadStatuses } = useStatuses()
   const { data: statusList } = statuses
 
-  const [authorizedStatuses, setAuthorizedStatuses] = useState([])
-
   const { trainings, load: loadTrainings } = useTrainings()
   const { data, isLoading } = trainings
 
-  const { apiMessage } = useApiMessages()
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
     setPage('My pending tasks')
@@ -81,14 +129,17 @@ const PendingTasks = () => {
       const authorizedStatuses = statusList.rows.filter((s) =>
         matchRoleStatus(user.roles, s.id)
       )
-      setAuthorizedStatuses(authorizedStatuses)
+      dispatch({
+        type: REDUCER_TYPE.AUTHORIZED_STATUSES,
+        payload: authorizedStatuses
+      })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [statusList.rows])
 
   useEffect(() => {
-    if (authorizedStatuses.length) {
-      const selectedStatuses = authorizedStatuses.map((status) =>
+    if (state.authorizedStatuses.length) {
+      const selectedStatuses = state.authorizedStatuses.map((status) =>
         parseInt(status.id, 10)
       )
 
@@ -99,38 +150,39 @@ const PendingTasks = () => {
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authorizedStatuses.length, pagination])
+  }, [state.authorizedStatuses.length, state.pagination])
 
   useEffect(() => {
-    if (!showInputParameters) handleConfirm()
+    if (!state.showInputParameters) handleConfirm()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedStatuses])
 
   useEffect(() => {
-    if (radioSelected === RADIO.NONE) {
-      setSelectedRows([])
+    if (state.selectedRadioOption === RADIO.NONE) {
+      dispatch({ type: REDUCER_TYPE.SELECTED_ROWS, payload: [] })
     } else {
-      setSelectedRows(
-        data.rows.filter((row) =>
-          authorizedStatuses.find((r) => r.id === row.status)
+      dispatch({
+        type: REDUCER_TYPE.SELECTED_ROWS,
+        payload: data.rows.filter((row) =>
+          state.authorizedStatuses.find((r) => r.id === row.status)
         )
-      )
+      })
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [radioSelected])
+  }, [state.selectedRadioOption])
 
   useEffect(() => {
     const payload = {
       date: formatYMD(date),
       statuses: selectedStatuses.join('-'),
-      pagination
+      pagination: state.pagination
     }
 
     loadTrainings(payload)
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [refresh])
+  }, [state.refresh])
 
   const handleView = (training) => {
     navigate(`/training/${training}`)
@@ -138,7 +190,7 @@ const PendingTasks = () => {
 
   const handleSelectedDateView = (e) => {
     e.preventDefault()
-    setShowInputParameters(true)
+    dispatch({ type: REDUCER_TYPE.SHOW_INPUT_PARAMS, payload: true })
   }
 
   const setToday = (e) => {
@@ -169,25 +221,25 @@ const PendingTasks = () => {
   const handleConfirm = (e) => {
     e?.preventDefault()
     if (selectedStatuses?.length) {
-      setRefresh((s) => !s)
-      setShowInputParameters(false)
+      dispatch({ type: REDUCER_TYPE.REFRESH, payload: !state.refresh })
+      dispatch({ type: REDUCER_TYPE.SHOW_INPUT_PARAMS, payload: false })
     }
   }
 
   const handleRadioButtonsChange = (option) => {
-    setRadioSelected(option)
+    dispatch({ type: REDUCER_TYPE.SELECTED_RADIO, payload: option })
   }
 
   const buildPayload = (status) => ({
-    records: selectedRows.map((r) => [r.id, status, user.id])
+    records: state.selectedRows.map((r) => [r.id, status, user.id])
   })
 
   const handleApprove = () => {
     approveMultiple(buildPayload(8))
       .then((res) => {
         apiMessage(res)
-        setRadioSelected(RADIO.NONE)
-        setRefresh((s) => !s)
+        dispatch({ type: REDUCER_TYPE.SELECTED_RADIO, payload: RADIO.NONE })
+        dispatch({ type: REDUCER_TYPE.REFRESH, payload: !state.refresh })
       })
       .catch((e) => apiMessage(e))
   }
@@ -196,8 +248,8 @@ const PendingTasks = () => {
     rejectMultiple(buildPayload(TRAINING_STATUS.CANCELLED))
       .then((res) => {
         apiMessage(res)
-        setRadioSelected(RADIO.NONE)
-        setRefresh((s) => !s)
+        dispatch({ type: REDUCER_TYPE.SELECTED_RADIO, payload: RADIO.NONE })
+        dispatch({ type: REDUCER_TYPE.REFRESH, payload: !state.refresh })
       })
       .catch((e) => apiMessage(e))
   }
@@ -209,16 +261,16 @@ const PendingTasks = () => {
   return (
     <main className="container-fluid pending-tasks">
       {isLoading && <Loading />}
-      {!showInputParameters && (
+      {!state.showInputParameters && (
         <SelectedDateView date={date} onClick={handleSelectedDateView} />
       )}
-      {showInputParameters && (
+      {state.showInputParameters && (
         <InputParameters
           onCalendarChange={handleCalendarChange}
           onStatusChange={handleStatusChange}
           date={date}
           setToday={setToday}
-          statuses={authorizedStatuses}
+          statuses={state.authorizedStatuses}
           selectedStatuses={selectedStatuses}
           onConfirm={handleConfirm}
         />
@@ -227,22 +279,26 @@ const PendingTasks = () => {
       {data.count > 0 && (
         <SelectAllRadioButtons
           onChange={handleRadioButtonsChange}
-          selected={radioSelected}
+          selected={state.selectedRadioOption}
         />
       )}
 
       <CardList
         Card={Card}
         data={data}
-        pagination={pagination}
-        onPagination={setPagination}
+        pagination={state.pagination}
+        onPagination={(pagination) =>
+          dispatch({ type: REDUCER_TYPE.PAGINATION, payload: pagination })
+        }
         onView={handleView}
         isLoading={isLoading}
-        selectedItems={selectedRows}
-        setSelected={setSelectedRows}
+        selectedItems={state.selectedRows}
+        setSelected={(rows) =>
+          dispatch({ type: REDUCER_TYPE.SELECTED_ROWS, payload: rows })
+        }
       />
       <FloatingButtons
-        isVisible={selectedRows.length > 0}
+        isVisible={state.selectedRows.length > 0}
         onApprove={handleApprove}
         onReject={handleReject}
       />
