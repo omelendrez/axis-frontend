cc684d6093709f99a835199ad629d036307c1e8d
Omar Melendrez <omar.melendrez@gmail.com>
2023-10-19 11:06:20 -0300

    Implement delete training course for System Admin user

--- src/components/features/training/TrainingView.jsx
+++ src/components/features/training/TrainingView.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import { Photo, Learner } from '../learner/learner-view'
 import { Course, StatusStamp } from './training-view'
 import { Action } from './training-actions'
-import { Divider, Modal, TrainingForm } from '@/components'
-import { getTraining, undoLastApproval } from '@/services'
+import { Confirm, Divider, Modal, TrainingForm } from '@/components'
+import { deleteTraining, getTraining, undoLastApproval } from '@/services'
 import useApiMessages from '@/hooks/useApiMessages'
 import useUser from '@/hooks/useUser'
+import useConfirm from '@/hooks/useConfirm'
 
 import '../learner/learner-view/learner.css'
 import './trainingView.css'
-import { TRAINING_STATUS } from '@/helpers'
+
 import { useNavigate } from 'react-router-dom'
 import { RejectReason } from './training-view/RejectReason'
 
@@ -19,6 +20,9 @@ export const TrainingView = ({ training, onUpdate }) => {
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [isTrainingEdit, setIsTrainingEdit] = useState(false)
   const [trainingEditData, setTrainingEditData] = useState(null)
+
+  const { isConfirmOpen, confirmMessage, setMessage, closeConfirm } =
+    useConfirm()
 
   const navigate = useNavigate()
 
@@ -46,9 +50,39 @@ export const TrainingView = ({ training, onUpdate }) => {
     e.preventDefault()
 
     undoLastApproval(id)
-      .then((res) => {
+      .then(() => {
         const data = {
           message: `Status "${statusName}" has been reverted!`
+        }
+        apiMessage({ data })
+
+        onUpdate()
+      })
+      .catch((e) => apiMessage(e))
+      .finally(() => setIsSubmitting(false))
+  }
+
+  const handleDelete = (e) => {
+    e.preventDefault()
+
+    const message = (
+      <span>
+        Are you sure you want to delete this training record
+        <span className="primary"> {training?.course?.name}</span>?
+      </span>
+    )
+
+    setMessage(message)
+  }
+
+  const handleDeleteConfirm = (e) => {
+    e.preventDefault()
+    closeConfirm()
+
+    deleteTraining(id)
+      .then(() => {
+        const data = {
+          message: 'Training record has been deleted!'
         }
         apiMessage({ data })
 
@@ -78,6 +112,12 @@ export const TrainingView = ({ training, onUpdate }) => {
     setIsTrainingEdit(false)
   }
 
+  const handleCancel = (e) => {
+    e.preventDefault()
+
+    closeConfirm()
+  }
+
   return (
     <main className="training-view">
       {reason && <RejectReason reason={reason} />}
@@ -89,7 +129,8 @@ export const TrainingView = ({ training, onUpdate }) => {
       />
       <Course
         training={training}
-        onUndo={isAdmin && statusId > TRAINING_STATUS.NEW ? handleUndo : null}
+        onUndo={isAdmin ? handleUndo : null}
+        onDelete={isAdmin ? handleDelete : null}
         isSubmitting={isSubmitting}
         onUpdate={onUpdate}
         onEdit={handleEditTraining}
@@ -101,6 +142,12 @@ export const TrainingView = ({ training, onUpdate }) => {
       <Modal open={isTrainingEdit} title="Edit training" onClose={handleClose}>
         <TrainingForm training={trainingEditData} onClose={handleClose} />
       </Modal>
+      <Confirm
+        open={isConfirmOpen}
+        onCofirm={handleDeleteConfirm}
+        onCancel={handleCancel}
+        message={confirmMessage}
+      />
     </main>
   )
 }
