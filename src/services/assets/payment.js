124ead94a7b75fadf3f41d301f92279986ad84b2
Omar Melendrez <melendrez.dev@gmail.com>
2023-11-25 12:35:52 -0300

    Convert image filename to uppercase

--- src/services/assets/payment.js
+++ src/services/assets/payment.js
@@ -6,10 +6,12 @@ const url = import.meta.env.VITE_ASSETS_URL
 const endpoint = 'payments'
 
 export const getPaymentUrl = (data) =>
-  `${url}${endpoint}/${getPaymentFilename(data)}?${new Date().getTime()}`
+  `${url}${endpoint}/${getPaymentFilename(
+    data
+  ).toUpperCase()}?${new Date().getTime()}`
 
 export const uploadPayment = (formData) =>
   api.post(`${url}${endpoint}`, formData)
 
 export const paymentExists = (data) =>
-  api.get(`${endpoint}/${getPaymentFilename(data)}/exists`)
+  api.get(`${endpoint}/${getPaymentFilename(data).toUpperCase()}/exists`)
