b9749ff50cf88c5a8df79479fc220c888096d415
Omar Melendrez <omar.melendrez@gmail.com>
2023-12-23 02:03:15 -0300

    change vite_assets_source

--- src/services/assets/common.js
+++ src/services/assets/common.js
@@ -1,7 +1,7 @@
 import { api } from './assetsClient'
 
 export const getBucketDocumentUrl = (url) => {
-  if (import.meta.env.VITE_ASSETS_SOURCE === 'LOCAL') {
+  if (import.meta.env.VITE_ASSETS_SOURCE === 'local') {
     return new Promise((resolve) => resolve({ data: url }))
   } else {
     return api.get(url)
