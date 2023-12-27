90ad01cc6ff7550684fdfd4147d9a5c36c1e711a
Omar Melendrez <omar.melendrez@gmail.com>
2023-12-27 13:48:21 -0300

    Remove forced cache

--- src/hooks/useCourses.js
+++ src/hooks/useCourses.js
@@ -6,20 +6,13 @@ import {
   removeCourse
 } from '../reducers/course/courseSlice'
 
-let coursesParams = null
-
 const useCourses = () => {
   const dispatch = useDispatch()
   const courses = useSelector((state) => state.courses)
 
   const add = (payload) => dispatch(addCourse(payload))
   const modify = (id, payload) => dispatch(modifyCourse(id, payload))
-  const load = (params) => {
-    if (coursesParams !== JSON.stringify(params)) {
-      dispatch(loadCourses(params))
-      coursesParams = JSON.stringify(params)
-    }
-  }
+  const load = (params) => dispatch(loadCourses(params))
   const remove = (id) => dispatch(removeCourse(id))
 
   return {
--- src/hooks/useStates.js
+++ src/hooks/useStates.js
@@ -6,20 +6,13 @@ import {
   removeState
 } from '../reducers/state/stateSlice'
 
-let statesParams = null
-
 const useStates = () => {
   const dispatch = useDispatch()
   const states = useSelector((state) => state.states)
 
   const add = (payload) => dispatch(addState(payload))
   const modify = (id, payload) => dispatch(modifyState(id, payload))
-  const load = (params) => {
-    if (statesParams !== JSON.stringify(params)) {
-      dispatch(loadStates(params))
-      statesParams = JSON.stringify(params)
-    }
-  }
+  const load = (params) => dispatch(loadStates(params))
   const remove = (id) => dispatch(removeState(id))
 
   return {
